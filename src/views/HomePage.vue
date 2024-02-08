6472e219ab2ca91e7aa950e8b9148c0a5e554bd7
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-02-08 21:00:09 +0300

    Reproduce the issue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,113 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
+    <ion-page ref="page">
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>App</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <ion-button id="open-modal" expand="block">Open</ion-button>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+            <ion-modal ref="modal" trigger="open-modal" :presenting-element="presentingElement" :initial-breakpoint="1" :breakpoints="[0, 1]">
+                <ion-header>
+                    <ion-toolbar>
+                        <ion-title>Modal</ion-title>
+                        <ion-buttons slot="end">
+                            <ion-button @click="dismiss()">Close</ion-button>
+                        </ion-buttons>
+                    </ion-toolbar>
+                </ion-header>
+                <ion-content class="ion-padding">
+                    <ion-list>
+                        <ion-item>
+                            <ion-avatar slot="start">
+                                <ion-img src="https://i.pravatar.cc/300?u=b"></ion-img>
+                            </ion-avatar>
+                            <ion-label>
+                                <h2>Connor Smith</h2>
+                                <p>Sales Rep</p>
+                            </ion-label>
+                        </ion-item>
+                        <ion-item>
+                            <ion-avatar slot="start">
+                                <ion-img src="https://i.pravatar.cc/300?u=a"></ion-img>
+                            </ion-avatar>
+                            <ion-label>
+                                <h2>Daniel Smith</h2>
+                                <p>Product Designer</p>
+                            </ion-label>
+                        </ion-item>
+                        <ion-item>
+                            <ion-avatar slot="start">
+                                <ion-img src="https://i.pravatar.cc/300?u=d"></ion-img>
+                            </ion-avatar>
+                            <ion-label>
+                                <h2>Greg Smith</h2>
+                                <p>Director of Operations</p>
+                            </ion-label>
+                        </ion-item>
+                        <ion-item>
+                            <ion-avatar slot="start">
+                                <ion-img src="https://i.pravatar.cc/300?u=e"></ion-img>
+                            </ion-avatar>
+                            <ion-label>
+                                <h2>Zoey Smith</h2>
+                                <p>CEO</p>
+                            </ion-label>
+                        </ion-item>
+                    </ion-list>
+                </ion-content>
+            </ion-modal>
+        </ion-content>
+    </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script lang="ts">
+import {
+    IonButtons,
+    IonButton,
+    IonModal,
+    IonHeader,
+    IonContent,
+    IonToolbar,
+    IonTitle,
+    IonItem,
+    IonList,
+    IonAvatar,
+    IonImg,
+    IonLabel,
+    IonPage,
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    components: {
+        IonButtons,
+        IonButton,
+        IonModal,
+        IonHeader,
+        IonContent,
+        IonToolbar,
+        IonTitle,
+        IonItem,
+        IonList,
+        IonAvatar,
+        IonImg,
+        IonLabel,
+        IonPage,
+    },
+    data() {
+        return {
+            presentingElement: null,
+        };
+    },
+    methods: {
+        dismiss() {
+            this.$refs.modal.$el.dismiss();
+        },
+    },
+    mounted() {
+        this.presentingElement = this.$refs.page.$el;
+    },
+});
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
